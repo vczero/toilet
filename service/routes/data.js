67e975b7a34da7fd887880403b4f5d28586190cc
lh_wang <lh_wang@Ctrip.com>
2016-07-30 10:46:42 +0800

    增加注释和鉴权

--- service/routes/data.js
+++ service/routes/data.js
@@ -3,53 +3,69 @@ var router = express.Router();
 var fs = require('fs');
 var PATH = './public/data/';
 
-//读取JSON数据
+//读取数据模块，供客户端调用
+//查询接口，token校验
+//公共接口，无需校验
+//data/read?type=it
+//data/read?type=it.json
 router.get('/read', function(req, res, next) {
   var type = req.param('type') || '';
-  fs.readFile(PATH + type + '.json', (err, data) => {
-    if (err) {
-     return res.send({
-       status:0,
-       info: 'fail.....'
-     });
+  fs.readFile(PATH + type + '.json', function(err, data){
+    if(err){
+      return res.send({
+        status:0,
+        info:'读取文件出现异常'
+      });
     }
     var COUNT = 50;
-    var obj = JSON.parse(data.toString());
+    //TODO: try
+    var obj = [];
+    try{
+      obj = JSON.parse(data.toString());
+    }catch(e){
+      obj = [];
+    }
     if(obj.length > COUNT){
       obj = obj.slice(0, COUNT);
     }
     return res.send({
       status:1,
-      data: obj
+      data:obj
     });
   });
 });
 
-//写入JSON数据
-router.post('/write', function(req, res, next) {
+//数据存储模块 后台开发使用
+router.post('/write', function(req, res, next){
+  if(!req.session.user){
+    return res.send({
+      status: 0,
+      info: '未鉴权认证'
+    });
+  }
   //文件名
   var type = req.param('type') || '';
   //关键字段
   var url = req.param('url') || '';
   var title = req.param('title') || '';
   var img = req.param('img') || '';
-
   if(!type || !url || !title || !img){
     return res.send({
-      status: 0,
-      info: '提交字段不全'
+      status:0,
+      info:'提交的字段不全'
     });
   }
-
-  fs.readFile(PATH + type + '.json', (err, data) => {
-    //读取
-    if (err) {
+  //1)读取文件
+  var filePath = PATH + type + '.json';
+  fs.readFile(filePath, function(err, data){
+    if(err){
       return res.send({
-        status: 0,
-        info: 'fail.....'
+        status:0,
+        info: '读取数据失败'
       });
     }
     var arr = JSON.parse(data.toString());
+    //代表每一条记录
     var obj = {
       img: img,
       url: url,
@@ -57,71 +73,83 @@ router.post('/write', function(req, res, next) {
       id: guidGenerate(),
       time: new Date()
     };
-
-    //写入
     arr.splice(0, 0, obj);
+    //2)写入文件
     var newData = JSON.stringify(arr);
-    fs.writeFile(PATH + type + '.json', newData, (err, data) => {
+    fs.writeFile(filePath, newData, function(err){
       if(err){
         return res.send({
-          status: 0,
-          info: 'fail.....'
+          status:0,
+          info: '写入文件失败'
         });
       }
       return res.send({
-        status: 1,
+        status:1,
         info: obj
       });
-
     });
   });
 });
 
-
-//写入阅读JSON数据
-router.post('/write_config', function(req, res, next) {
+//阅读模块写入接口 后台开发使用
+router.post('/write_config', function(req, res, next){
+  if(!req.session.user){
+    return res.send({
+      status: 0,
+      info: '未鉴权认证'
+    });
+  }
+  //TODO:后期进行提交数据的验证
+  //防xss攻击 xss
+  // npm install xss
+  // require('xss')
+  // var str = xss(name);
   var data = req.body.data;
+  //TODO ： try catch
   var obj = JSON.parse(data);
-  //写入
   var newData = JSON.stringify(obj);
-  fs.writeFile(PATH + 'config.json', newData, (err, data) => {
+  //写入
+  fs.writeFile(PATH + 'config.json', newData, function(err){
     if(err){
       return res.send({
         status: 0,
-        info: 'fail.....'
+        info: '写入数据失败'
       });
     }
     return res.send({
       status: 1,
       info: obj
     });
-
   });
 });
 
-//登录
-router.post('/login', function(req, res, next) {
+//登录接口
+router.post('/login', function(req, res, next){
+  //用户名、密码、验证码
   var username = req.body.username;
   var password = req.body.password;
 
+  //TODO ：对用户名、密码进行校验
+  //xss处理、判空
+
+  //密码加密 md5(md5(password + '随机字符串'))
+  //密码需要加密－> 可以写入JSON文件
   if(username === 'admin' && password === '123456'){
     req.session.user = {
-      username: username,
-      ok: '@#$%&^@@'
+      username: username
     };
     return res.send({
       status: 1
     });
-  }else{
-    return res.send({
-      status: 0,
-      info: '登录失败'
-    });
-
   }
+
+  return res.send({
+    status: 0,
+    info: '登录失败'
+  });
 });
 
-
+//guid
 function guidGenerate() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0,
